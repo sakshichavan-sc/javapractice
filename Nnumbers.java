b1a56ee152912d3f4b71a2bf7507caf8278d0915
sakshi c <sakshichavan.sn@gmail.com>
2026-06-29 17:08:15 +0530

    todays practice1

---
+++ Nnumbers.java
@@ -0,0 +1,19 @@
+//Print Numbers from 1 to N
+
+import java.util.Scanner;
+
+public class Nnumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("enetr a n");
+         int n = sc.nextInt();
+
+         for(int i=1; i<=n;i++){
+            System.out.println(i + " ");
+
+         }
+
+
+    }
+    
+}
